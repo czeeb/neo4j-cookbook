4bf1a773c4f9288dc42914956a7cb9b071ec1a67
Chris Zeeb <chris.zeeb@gmail.com>
2015-09-03 14:59:42 -0400

    Updating version in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'Apache 2.0'
 description      'Installs/Configures neo4j'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url       'https://github.com/czeeb/neo4j-cookbook/issues'
-version          '0.1.0'
+version          '1.0.0'
 
 supports 'amazon'
 supports 'centos', '>= 6.6'
