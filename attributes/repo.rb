80f0e5a8cad4b347647fc30171a06e675e2f3049
Mark Selby <mselby@tokenrain.net>
2016-09-09 18:46:06 -0700

    add distribution as an attribute for apt packages as newest chef client adds a default if left full unset

--- attributes/repo.rb
+++ attributes/repo.rb
@@ -8,6 +8,7 @@ default['neo4j']['yum']['mirrorlist'] = nil
 default['neo4j']['yum']['action'] = :create
 
 default['neo4j']['apt']['description'] = 'Neo4j Repository'
+default['neo4j']['apt']['distribution'] = nil
 default['neo4j']['apt']['components'] = ['stable/']
 default['neo4j']['apt']['action'] = :add
 default['neo4j']['apt']['uri'] = 'http://debian.neo4j.org/repo'
